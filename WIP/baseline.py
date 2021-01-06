5d4f9660aa3d4e67c9447b9dcdf5864de9412025
ElysiaTanZY <elysiatan97@gmail.com>
2021-01-06 15:25:40 +0800

    Updated baseline model to include beta calculation and test for time series stationarity prior to cointegration test

--- WIP/baseline.py
+++ WIP/baseline.py
@@ -1,83 +1,9 @@
 import json
-import pandas as pd
+import math
 import matplotlib as plt
+import pandas as pd
 import statsmodels.tsa.stattools as st
-
-
-shares = []
-
-
-def get_shares(data):
-    # Get the code of all shares that are available in the exchanges
-    global shares
-    curr_share = data.iloc[0]['PERMNO']
-    share_list = []
-
-    for j, row in data.iterrows():
-        if row['PERMNO'] != curr_share:
-            share_list.append((curr_share, row['SICCD']))
-            curr_share = row['PERMNO']
-
-    shares.append(share_list)
-    print(share_list)
-
-
-def find_shares(_start_year_form, data):
-    # Sieves out all stocks that have prices for all days within the formation period
-    chosen_shares = []
-    start_date = pd.datetime.date(_start_year_form, 1, 1)
-    if start_date.dt.dayofweek == 7:
-        start_date.apply(pd.DateOffset(1))
-    elif start_date.dt.dayofweek == 6:
-        start_date.apply(pd.DateOffset(2))
-
-    end_date = pd.datetime.date(_start_year_form + 2, 12, 31)
-
-    if end_date.dt.dayofweek == 7:
-        end_date.apply(pd.DateOffset(-2))
-    elif end_date.dt.dayofweek == 6:
-        end_date.apply(pd.DateOffset(-1))
-
-    curr_date = start_date
-    curr_share = shares[0]
-    count = 1
-    start_index = 0
-    is_start = False
-
-    for j, row in data.iterrows():
-        if row['PERMNO'] == curr_share[0]:
-            if is_start:
-                start_index = j
-
-            if row['date'] == curr_date:
-                if curr_date.dt.dayofweek == 5:
-                    curr_date.apply(pd.DateOffset(3))
-                else:
-                    curr_date.apply(pd.DateOffset(1))
-
-            elif row['date'] == end_date:
-                chosen_shares.append([curr_share[0], curr_share[1], start_index, j])
-
-                if count < len(shares):
-                    curr_share = shares[count]
-                    count += 1
-                    curr_date = start_date
-                    is_start = True
-                else:
-                    break
-
-            else:
-                if count < len(shares):
-                    curr_share = shares[count]
-                    count += 1
-                    curr_date = start_date
-                    is_start = True
-                else:
-                    break
-
-        print(chosen_shares)
-
-    return chosen_shares
+import statsmodels.api as sm
 
 
 def group_shares(relevant_shares):
@@ -91,34 +17,32 @@ def group_shares(relevant_shares):
 
         if sic_code == 9999:
             name = 'unclassified'
+            update_groups(groups, name, relevant_shares, i)
 
-            if name not in groups.keys():
-                groups[name] = [(relevant_shares[i][0], relevant_shares[i][2], relevant_shares[i][3])]
-            else:
-                temp = groups[name]
-                temp.append((relevant_shares[i][0], relevant_shares[i][2], relevant_shares[i][3]))
-                groups[name] = temp
+        else:
+            for key, value in data.items():
+                start = value['start']
+                end = value['end']
 
-        for key, value in data.items():
-            start = value['start']
-            end = value['end']
-
-            if start <= sic_code <= end:
-                if key not in groups.keys():
-                    groups[key] = [(relevant_shares[i][0], relevant_shares[i][2], relevant_shares[i][3])]
-                else:
-                    temp = groups[key]
-                    temp.append((relevant_shares[i][0], relevant_shares[i][2], relevant_shares[i][3]))
-                    groups[key] = temp
-
-            break
+                if start <= sic_code <= end:
+                    update_groups(groups, key, relevant_shares, i)
+                break
 
     return groups
 
 
+def update_groups(groups, name, relevant_shares, index):
+    if name not in groups.keys():
+        groups[name] = [(relevant_shares[index][0], relevant_shares[index][2], relevant_shares[index][3])]
+    else:
+        temp = groups[name]
+        temp.append((relevant_shares[index][0], relevant_shares[index][2], relevant_shares[index][3]))
+        groups[name] = temp
+
+
 def form_pairs(grouped_shares):
     # Generate all the relevant pairs
-    pairs = {}  # group: (pair 1, pair 2)
+    pairs = {}  # group: [(pair 1, pair 2)]
 
     for group, share_list in grouped_shares.items():
         pairs[group] = []
@@ -132,38 +56,74 @@ def form_pairs(grouped_shares):
     return pairs
 
 
-def cointegration(_ranked_pairs, data):
+def find_cointegrated_pairs(potential_pairs, data):
     # Test pairs for cointegration and return cointegrated pairs
     # Normalise price series before testing for cointegration
-    # TODO: Heat map??? and stop once hit 20 actually check how many pairs are there in total and decide number from there
+    # TODO: Heat map???
+    # TODO: Standardise price series
     selected_pairs = []
 
     relevant_cols = ['date', 'LOG_PRC']
 
-    for key, pairs in _ranked_pairs.items():
+    for key, pairs in potential_pairs.items():
         for i in range(0, len(pairs)):
             pair_one, pair_two = pairs[i]       # Share code, start row, end row
 
             pair_one_prices = data.iloc[pair_one[1]:pair_one[2] + 1]
+            pair_one_prices['LOG_PRC'] = pair_one_prices.apply(lambda row: math.log(row['PRC'], 10), axis = 1)
             pair_one_price_series = pd.DataFrame(pair_one_prices, columns=relevant_cols)
 
             pair_two_prices = data.iloc[pair_two[1]:pair_two[2] + 1]
+            pair_two_prices['LOG_PRC'] = pair_two_prices.apply(lambda row: math.log(row['PRC'], 10), axis=1)
             pair_two_price_series = pd.DataFrame(pair_two_prices, columns=relevant_cols)
 
+            '''
             # Plot time series
             ax = pair_one_price_series.plot(x='date', y='LOG_PRC')
             pair_two_price_series.plot(ax=ax)
             plt.show()
+            '''
 
-            # Test for co-integration
+            '''
+            Test for stationarity
+            ADF Test: Fail to reject null hypothesis --> Non-stationary (p-value > 0.05)
+            '''
+            adf_result_one = st.adfuller(pair_one_price_series)
+            adf_result_two = st.adfuller(pair_two_price_series)
+
+            if adf_result_one[1] <= 0.05 or adf_result_two <= 0.05:
+                continue
+
+            '''
+            Test for co-integration
+            Engle and Granger: Reject null hypothesis --> Co-integrated (p-value < 0.05)
+            '''
             coint_t, pvalue, crit_value = st.coint(pair_one_price_series['LOG_PRC'], pair_two_price_series['LOG_PRC'])
 
             if pvalue < 0.05:
-                spread = pair_one_price_series['LOG_PRC'] - pair_two_price_series['LOG_PRC']
-                mean = spread.mean()
-                std = spread.std()
-                pair = (pair_one[0], pair_one[2] + 1, pair_two[0], pair_two[2] + 1, mean, std)
-                selected_pairs.append(pair)
+                pair_two_price_series = sm.add_constant(pair_two_price_series)
+                model = sm.OLS(pair_one_price_series, pair_two_price_series)
+                results = model.fit()
+                intercept, beta = results.params
+
+                if beta > 0:
+                    pair_two_price_series['LOG_PRC'] = pair_two_price_series['LOG_PRC'].apply(lambda x:x*beta)
+                    spread = pair_one_price_series['LOG_PRC'] - pair_two_price_series['LOG_PRC']
+                    mean = spread.mean()
+                    std = spread.std()
+                    pair = (pair_one[0], pair_one[2] + 1, pair_two[0], pair_two[2] + 1, mean, std, beta)
+                    selected_pairs.append(pair)
+                else:
+                    pair_one_price_series = sm.add_constant(pair_one_price_series)
+                    model = sm.OLS(pair_two_price_series, pair_one_price_series)
+                    results = model.fit()
+                    intercept, beta = results.params
+
+                    if beta > 0:
+                        mean = spread.mean()
+                        std = spread.std()
+                        pair = (pair_two[0], pair_two[2] + 1, pair_one[0], pair_one[2] + 1, mean, std, beta)
+                        selected_pairs.append(pair)
 
     return selected_pairs
 
@@ -184,14 +144,12 @@ def rank_pairs(_possible_pairs, data):
     return
 '''
 
-def main(start_year, data):
-    # Formation period: 3 years + Trading period: 1 year (Rolling basis)
-    # Start date: 1 Jan 2000
-    get_shares(data)
 
-    relevant_shares = find_shares(start_year, data)
+def main(data, relevant_shares):
+    # Formation period: 3 years + Trading period: 1 year (Rolling basis)
     grouped_shares = group_shares(relevant_shares)
     possible_pairs = form_pairs(grouped_shares)         # group: (share code, start row, end row)
     #ranked_pairs = rank_pairs(possible_pairs, data)
-    cointegrated_pairs = cointegration(possible_pairs, data)   # group: (pair_1, pair_2)
+
+    cointegrated_pairs = find_cointegrated_pairs(possible_pairs, data)   # [(permno_one, trade_start_one_row, permno_two, trade_start_two_row, mean, std, beta)]
     return cointegrated_pairs
