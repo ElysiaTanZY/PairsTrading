3ba47c93b4951e9bebe9d0920f11b6c1013e6d55
ElysiaTanZY <elysiatan97@gmail.com>
2021-01-11 10:43:59 +0800

    Update algo to calculate Maximum Drawdown

--- WIP/analyse_returns.py
+++ WIP/analyse_returns.py
@@ -9,53 +9,74 @@ def analyse_returns(returns, num_pairs_traded, num_pairs_chosen):
     fully_invested_return_list = []
 
     for i in range(0, len(returns)):
-        return_on_committed_capital_list.append(sum(returns) / num_pairs_chosen if num_pairs_chosen > 0 else 0)
-        fully_invested_return_list.append(sum(returns) / num_pairs_traded if num_pairs_traded > 0 else 0)
+        return_on_committed_capital_list.append(returns[i] / num_pairs_chosen[i] if num_pairs_chosen[i] > 0 else 0)
+        fully_invested_return_list.append(returns[i] / num_pairs_traded[i] if num_pairs_traded[i] > 0 else 0)
 
     return_on_committed_capital = statistics.mean(return_on_committed_capital_list)
     fully_invested_return = statistics.mean(fully_invested_return_list)
 
+    print("Return on committed capital: " + str(return_on_committed_capital))
+    print("Fully invested return: " + str(fully_invested_return))
+
     # Maximum Drawdown
-    peak = max(returns)
-    trough = min(returns)
-    mdd = (trough - peak) / peak
+    peak = 0.0
+    gap = 0.0
+
+    for i in range(0, len(returns)):
+        for j in range(i + 1, len(returns)):
+            temp_gap = returns[j] - returns[i]
+
+            if (temp_gap < gap):
+                gap = temp_gap
+                peak = returns[i]
+
+    mdd = gap / peak if peak > 0.0 else 0
+
+    print("Maximum drawdown: " + str(mdd))
 
     # Sharpe ratio
-    average = statistics.mean(returns)
-    sd = statistics.stdev(returns)
+    average = statistics.mean(return_on_committed_capital_list)
+    sd = statistics.stdev(return_on_committed_capital_list, average)
     risk_free_rate = generate_risk_free_rate()  # Using average of 1-year treasury yield over past 17 years
-    sharpe_ratio = (average - risk_free_rate) / sd
+    sharpe_ratio = (average - risk_free_rate) / sd if sd > 0.0 else 0
+
+    print("Sharpe ratio: " + str(sharpe_ratio))
 
     # Calmar ratio
-    calmar_ratio = average / mdd
+    calmar_ratio = average / mdd if mdd > 0.0 else 0
+
+    print("Calmar ratio: " + str(calmar_ratio))
 
     # Sortino ratio (https://www.thebalance.com/what-is-downside-deviation-4590379)
     minimal_acceptable_return = average
 
     downside_returns = []
-    for i in range(0, len(returns)):
-        returns_subtract_mar = returns[i] - minimal_acceptable_return
+    for i in range(0, len(return_on_committed_capital_list)):
+        returns_subtract_mar = return_on_committed_capital_list[i] - minimal_acceptable_return
 
         if returns_subtract_mar < 0:
             downside_returns.append(returns_subtract_mar**2)
 
-    downside_deviation = math.sqrt(sum(downside_returns) / len(returns))
-    sortino_ratio = (average - risk_free_rate) / downside_deviation
+    downside_deviation = math.sqrt(sum(downside_returns) / len(return_on_committed_capital_list)) if len(return_on_committed_capital_list) > 0 else 0
+    sortino_ratio = (average - risk_free_rate) / downside_deviation if downside_deviation > 0.0 else 0
+
+    print("Sortino ratio: " + str(sortino_ratio))
 
     return
 
 
 def generate_risk_free_rate():
-    rates_data = pd.read_csv('../Backup/1yr_TreasuryYield(Daily).csv')  # https://fred.stlouisfed.org/series/DGS1
-    curr_year = rates_data[0]['DATE'].dt.year
-    rates = [rates_data[0]['Treasury Yield']]
+    date_cols = ['DATE']
+    rates_data = pd.read_csv('../Backup/1yr_TreasuryYield(Daily).csv', parse_dates=date_cols)  # https://fred.stlouisfed.org/series/DGS1
+    curr_year = rates_data.iloc[0]['DATE'].year
+    rates = [rates_data.iloc[0]['Treasury Yield']]
 
     for i, row in rates_data.iterrows():
-        if row['DATE'].dt.year != curr_year:
-            curr_year = row['DATE'].dt.year
+        if row['DATE'].year != curr_year or i == len(rates_data) - 1:
+            curr_year = row['DATE'].year
             rates.append(row['Treasury Yield'])
 
-    return statistics.mean(rates)
+    return statistics.mean(rates) / 100
 
 
 if __name__ == '__main__':
