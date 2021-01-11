485e7907aeea29ab4f41d27cbb222d4220bd170d
ElysiaTanZY <elysiatan97@gmail.com>
2021-01-11 10:42:20 +0800

    Change early termination criteria to: Number of data rows for that share for that year != Number of trading days for the year

--- WIP/runner.py
+++ WIP/runner.py
@@ -23,13 +23,14 @@ trading_calendar = {2000: 252, 2001: 248, 2002: 252, 2003: 252, 2004: 252, 2005:
                    2009: 252, 2010: 252, 2011: 252, 2012: 250, 2013: 252, 2014: 252, 2015: 252, 2016: 252, 2017: 251,
                    2018: 251, 2019: 252}
 
+
 def consolidate_shares(data):
     # Get the code of all shares that are available at some point in time in the exchange and the corresponding
     # start and end index for each year
     global shares
 
     try:
-        with open('/Users/elysiatan/PycharmProjects/thesis/WIP/result.json') as json_file:
+        with open('../Backup/result.json') as json_file:
             shares = json.load(json_file)
             return
     except FileNotFoundError:
@@ -46,7 +47,7 @@ def consolidate_shares(data):
 
         if pd.notna(row['PERMNO']) and row['PERMNO'] != curr_share:
             temp = shares[curr_share]
-            temp[prev_year] = (start, j - 1)
+            temp[str(prev_year)] = (start, j - 1)
             shares[curr_share] = temp
 
             prev_year = curr_year
@@ -56,12 +57,12 @@ def consolidate_shares(data):
 
         elif curr_year != prev_year:
             temp = shares[curr_share]
-            temp[prev_year] = (start, j - 1)
+            temp[str(prev_year)] = (start, j - 1)
             shares[curr_share] = temp
             prev_year = curr_year
             start = j
 
-    with open('result.json', 'w') as fp:
+    with open('../Backup/result.json', 'w') as fp:
         json.dump(shares, fp)
 
 
@@ -74,39 +75,23 @@ def find_shares(start_year):
         if str(start_year) not in dates.keys() or str(start_year + 1) not in dates.keys() or \
                 str(start_year + 2) not in dates.keys() or str(start_year + 3) not in dates.keys():
             continue
-
+        print("I am here")
         is_complete = True
+
         for i in range(start_year, start_year + 3):
             start, end = dates[str(i)]
 
+            print(start)
+            print(end)
+
             if (end - start) < (trading_calendar[i] - 1):
                 # Missing prices in between
+                print("Missing prices in between")
                 is_complete = False
                 break
 
-            end_date = datetime.datetime(i, 12, 31)
-
-            if end_date.weekday() == 6:                # Monday = 0, Sunday = 6
-                end_date = end_date - datetime.timedelta(days=2)
-            elif end_date.weekday() == 5:
-                end_date = end_date - datetime.timedelta(days=1)
-
-            end_date = pd.to_datetime(end_date)
-
-            start_date = datetime.datetime(i, 1, 2)
-
-            if start_date.weekday() == 6:  # Monday = 0, Sunday = 6
-                start_date = start_date + datetime.timedelta(days=1)
-            elif start_date.weekday() == 5:
-                start_date = start_date + datetime.timedelta(days=2)
-
-            start_date = pd.to_datetime(start_date)
-
             indicator = True
             for j in range(start, end + 1):
-                if j == start and not data.iloc[j]['date'] == start_date:
-                    indicator = False
-                    break
 
                 if not data.iloc[j]['PRC'] > 1.0 or not 100 <= data.iloc[j]['DLSTCD'] < 200 or not int(data.iloc[j]['VOL']) > 0:
                     '''
@@ -114,13 +99,11 @@ def find_shares(start_year):
                     2. Discard the stock when its price is less than $1 to avoid relatively high trading costs and 
                     complications (Faff et al, 2016) 
                     3. Discard the stock when its trading volume is 0 to replicate practical trading environments and 
-                    deal with only liquid stocks(Faff et al, 2016)
+                    deal with only liquid stocks (Faff et al, 2016)
                     '''
+                    print("Price less than 1 or delisted")
                     indicator = False
                     break
-
-                if j == end and not data.iloc[j]['date'] == end_date:
-                    indicator = False
 
             if not indicator:
                 is_complete = False
@@ -131,72 +114,22 @@ def find_shares(start_year):
             If the industry code of the PERMNO changes within the formation period, use the latest one to align with the 
             trading period
             '''
+            print("Appending")
             chosen_shares.append([permno, data.iloc[dates[str(start_year+2)][1]]['SICCD'], dates[str(start_year)][0], dates[str(start_year+2)][1]])
 
-    '''
-    start_date = pd.datetime.date(start_year, 1, 1)
-    if start_date.dt.dayofweek == 7:
-        start_date.apply(pd.DateOffset(1))
-    elif start_date.dt.dayofweek == 6:
-        start_date.apply(pd.DateOffset(2))
-
-    end_date = pd.datetime.date(start_year + 2, 12, 31)
-
-    if end_date.dt.dayofweek == 7:
-        end_date.apply(pd.DateOffset(-2))
-    elif end_date.dt.dayofweek == 6:
-        end_date.apply(pd.DateOffset(-1))
-
-    curr_date = start_date
-    curr_share = shares[0]
-    count = 1
-    start_index = 0
-    is_start = False
-
-    for j, row in data.iterrows():
-        if pd.notna(row['PERMNO']) and row['PERMNO'] == curr_share[0]:
-            if row['date'] == curr_date and bool(re.search('[1-9]+', str(row['PRC']))):
-                if is_start:
-                    start_index = j
-
-                if curr_date.dt.dayofweek == 5:
-                    curr_date.apply(pd.DateOffset(3))
-                else:
-                    curr_date.apply(pd.DateOffset(1))
-
-                if row['date'] == end_date and 100 <= row['DLSTCD'] < 200:
-                    chosen_shares.append([curr_share[0], curr_share[1], start_index, j])
-
-                    if count < len(shares):
-                        curr_share = shares[count]
-                        count += 1
-                        curr_date = start_date
-                        is_start = True
-            else:
-                if count < len(shares):
-                    curr_share = shares[count]
-                    count += 1
-                    curr_date = start_date
-                    is_start = True
-    '''
-    with open('chosen_shares.json', 'w') as fp:
+    with open('../Backup/chosen_shares.json', 'w') as fp:
         json.dump(chosen_shares, fp)
-    #print(chosen_shares)
+
     return chosen_shares
 
 
 if __name__ == '__main__':
     consolidate_shares(data)
-    relevant_shares = find_shares(2000)
-    #baseline_pairs = baseline.main(data, relevant_shares)
-    #returns, num_pairs_traded = trade.trade(baseline_pairs, 2003, data)
-    #relevant_shares = find_shares(2001)
 
     baseline_returns = []
     baseline_num_pairs_chosen = []
     baseline_num_pairs_traded = []
 
-    '''
     for start_year in range(2000, 2017):
         relevant_shares = find_shares(start_year)
 
@@ -208,4 +141,3 @@ if __name__ == '__main__':
         baseline_num_pairs_chosen.append(len(baseline_pairs))
 
     analyse_returns.analyse_returns(baseline_returns, baseline_num_pairs_traded, baseline_num_pairs_chosen)
-    '''
