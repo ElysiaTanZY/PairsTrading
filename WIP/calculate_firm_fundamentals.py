76049d00bc9bf93f1c479433a5ed5fe505d15127
ElysiaTanZY <elysiatan97@gmail.com>
2021-01-17 12:58:49 +0800

    Generate firm fundamentals required for clustering model

--- WIP/calculate_firm_fundamentals.py
+++ WIP/calculate_firm_fundamentals.py
@@ -1,49 +1,114 @@
 import datetime
 import pandas as pd
+import re
 
-# TODO: Add beta data
 
 def calculate_yearly():
     date_cols = ['datadate']
     compustat_file = "/Users/elysiatan/PycharmProjects/thesis/Backup/Firm_Fundamentals_Annual.csv"
-    compustat_field = ['datadate', 'LPERMNO', 'at', 'csho', 'ni', 'bkvlps', 'mkvalt', 'prcc_f', 'ceq', 'dvpa', 'tstkp']
-    compustat = pd.read_csv(compustat_file, usecols=compustat_field, parse_dates=date_cols)
+    compustat_fields = ['datadate', 'LPERMNO', 'at', 'csho', 'ni', 'lt', 'mkvalt', 'seq', 'prcc_f', 'ceq', 'dvpa', 'tstkp', 'ebit', 'txt', 'ch', 'dlc', 'dltt']
+    compustat = pd.read_csv(compustat_file, usecols=compustat_fields, parse_dates=date_cols)
 
     date_cols.clear()
     date_cols = ['date']
     crsp_file = "/Users/elysiatan/PycharmProjects/thesis/Updated/Data_NASDAQ.csv"
-    crsp_field = ['date', 'PERMNO', 'PRC', 'SHROUT']
-    crsp = pd.read_csv(crsp_file, usecols=crsp_field, parse_dates=date_cols)
+    crsp_fields = ['date', 'PERMNO', 'PRC', 'SHROUT']
+    crsp = pd.read_csv(crsp_file, usecols=crsp_fields, parse_dates=date_cols)
+    crsp.dropna(subset=['PERMNO'], inplace=True)
+    crsp['PERMNO'] = crsp['PERMNO'].astype(int)
+    print(compustat.dtypes)
+    print(crsp.dtypes)
 
+    # Nullified if net income is negative
     # ROE = Net Income / Shareholder's Equity
-    compustat['roe'] = compustat.apply(lambda row: row['ni'] / (row['bkvlps'] * row['csho']), axis=1)
+    compustat['roe'] = compustat.apply(lambda row: row['ni'] / (row['at'] - row['lt']) if
+        bool(re.search('[1-9]+', str(row['at'] - row['lt']))) and row['ni'] > 0 and row['at'] - row['lt'] > 0 else 0.0, axis=1)
+
+    print("done roe")
 
     # ROA = Net Income / Total Assets
-    compustat['roa'] = compustat.apply(lambda row: row['ni'] / row['at'], axis=1)
+    compustat['roa'] = compustat.apply(lambda row: row['ni'] / row['at'] if
+        bool(re.search('[1-9]+', str(row['at']))) and row['at'] > 0 and row['ni'] > 0 else 0.0, axis=1)
+
+    print("done roa")
+
+    # ROIC: NOPAT / Inveted Capital
+    compustat['nopat'] = compustat.apply(lambda row: row['ebit'] - row['txt'], axis=1)
+    compustat['invested_capital'] = compustat.apply(lambda row: row['dlc'] + row['dltt'] + row['seq'] - row['ch'], axis=1)
+    compustat['roic'] = compustat.apply(lambda row: row['nopat'] / row['invested_capital'] if
+        bool(re.search('[1-9]+', str(row['invested_capital']))) and row['nopat'] > 0 and row['invested_capital'] > 0 else 0.0, axis=1)
+
+    print('done roic')
 
     # Market Cap (Fiscal end) = Shares Outstanding * Share Price at fiscal end
     compustat['marketCap_Fiscal'] = compustat.apply(lambda row: row['csho'] * row['prcc_f'], axis=1)
 
-    # Market Cap (Calendar end) = Shares Outstanding * Share Price
-    crsp['marketCap_Calendar'] = crsp.apply(lambda row: row['PRC'] * row['SHROUT'], axis=1)
+    print('done market cap fiscal')
 
     # Book value of common equity = Common Equity + Preferred Treasury Stock - Preferred Dividends in Arrears
     # (The Book-to-Price Effect in Stock Returns: Accounting for Leverage)
-    compustat['Book_Value'] = compustat.apply(lambda row: row['ceq'] + row['tstkp'] - row['dvpa'], axis=1)
+    compustat['Book_Value_CE'] = compustat.apply(lambda row: row['ceq'] + row['tstkp'] - row['dvpa'], axis=1)
+
+    print('done book value')
+
+    compustat['BM_Ratio_Fiscal'] = compustat.apply(lambda row: row['Book_Value_CE'] / (row['marketCap_Fiscal']) if
+    bool(re.search('[1-9]+', str(row['marketCap_Fiscal']))) else 0.0, axis=1)
+
+    print('done b-m ratio fiscal')
 
     # Book-Market Ratio: Common Equity for the fiscal year ending in calendar year t-1 / Market Equity at the end of Dec
     # year t-1 (Size and Book-to-Market Factors in Earnings and Returns)
     # Book-Market Ratio: Common Shareholder's Equity / Market Cap
-    compustat['Book-Market_Ratio'], compustat['Book-Market_Ratio'] = compute_book_to_market(compustat, crsp)
+    compustat['marketCap_Calendar'], compustat['BM_Ratio_Calendar'] = retrieve_from_crsp(compustat, crsp)
 
-    result_list = ['datadate', 'LPERMNO', 'roe', 'roa', 'marketCap_Calendar', 'Book-Market_Ratio']
+    print('done market cap calendar and b-m ratio calendar')
+
+    # Beta: Retrieved from Beta Suite by WRDS
+    compustat['beta'] = retrieve_from_beta_suite(compustat)
+
+    print('done beta')
+
+    result_list = ['datadate', 'LPERMNO', 'roe', 'roa', 'roic', 'marketCap_Calendar', 'marketCap_Fiscal', 'BM_Ratio_Fiscal', 'BM_Ratio_Calendar', 'beta', 'Book_Value_CE', 'nopat', 'invested_capital']
     result = compustat[result_list]
 
     new_file_name = "/Users/elysiatan/PycharmProjects/thesis/Updated/Data_Firm.csv"
     result.to_csv(new_file_name)
 
 
-def compute_book_to_market(compustat, crsp):
+def retrieve_from_beta_suite(compustat):
+    # Retrieve market beta from beta suite dataset
+    beta = []
+
+    date_cols = ['DATE']
+    file = "/Users/elysiatan/PycharmProjects/thesis/Backup/beta_monthly.csv"
+    fields = ['DATE', 'PERMNO', 'b_mkt']
+    beta_suite = pd.read_csv(file, usecols=fields, parse_dates=date_cols)
+
+    for i, row in compustat.iterrows():
+        permno = row['LPERMNO']
+        year = row['datadate'].year
+
+        end_date = datetime.datetime(year, 12, 31)
+        if end_date.weekday() == 6:  # Monday = 0, Sunday = 6
+            end_date = end_date - datetime.timedelta(days=2)
+        elif end_date.weekday() == 5:
+            end_date = end_date - datetime.timedelta(days=1)
+
+        end_date = pd.to_datetime(end_date)
+
+        result = beta_suite.loc[beta_suite['PERMNO'] == permno]
+        result = result.loc[result['DATE'] == end_date]
+
+        if len(result.values) != 0:
+            beta.append(result['b_mkt'].values[0])
+        else:
+            beta.append(0.0)
+
+    return beta
+
+
+def retrieve_from_crsp(compustat, crsp):
+    # Retrieve market cap at calendar year end and calculate book/market ratio for given PERMNO for that year
     market_cap = []
     book_to_market = []
 
@@ -59,12 +124,53 @@ def compute_book_to_market(compustat, crsp):
 
         end_date = pd.to_datetime(end_date)
 
-        result_row = crsp.loc[crsp['PERMNO'] == permno and crsp['date'] == end_date]
-        market_cap.append(result_row['marketCap_Calendar'])
-        book_to_market.append(row['Book_Value'] / result_row['marketCap_Calendar'])
+        result = crsp.loc[crsp['PERMNO'] == permno]
+        result = result.loc[crsp['date'] == end_date]
+
+        if len(result.values) != 0 and bool(re.search('[1-9]+', str(result['PRC'].values[0]))) and \
+                bool(re.search('[1-9]+', str(result['SHROUT'].values[0]))):
+            market_cap.append(result['PRC'].values[0] * result['SHROUT'].values[0])
+            book_to_market.append((row['Book_Value_CE'] * ((result['SHROUT'].values[0] / 1000) / row['csho'])) / (result['PRC'].values[0] * (result['SHROUT'].values[0] / 1000)))
+        else:
+            market_cap.append(0.0)
+            book_to_market.append(0.0)
 
     return market_cap, book_to_market
 
 
+def check_yearly():
+    result_file = "/Users/elysiatan/PycharmProjects/thesis/Updated/Data_Firm.csv"
+    result = pd.read_csv(result_file)
+
+    result = result.drop(columns=['index'])
+
+    print(result[['roe', 'roa', 'roic']].describe())
+    print(result[result.roe == result.roe.min()])
+    print(result[result.roe == result.roe.max()])
+    print(result[result.roa == result.roa.min()])
+    print(result[result.roa == result.roa.max()])
+    print(result[result.roic == result.roic.min()])
+    print(result[result.roic == result.roic.max()])
+
+    print(result[['beta']].describe())
+
+    print(result[['Book_Value_CE']].describe())
+    print(result[result.Book_Value_CE == result.Book_Value_CE.min()])
+    print(result[result.Book_Value_CE == result.Book_Value_CE.max()])
+
+    print(result[['marketCap_Calendar', 'marketCap_Fiscal']].describe())
+    print(result[result.marketCap_Calendar == result.marketCap_Calendar.min()])
+    print(result[result.marketCap_Calendar == result.marketCap_Calendar.max()])
+    print(result[result.marketCap_Fiscal == result.marketCap_Fiscal.min()])
+    print(result[result.marketCap_Fiscal == result.marketCap_Fiscal.max()])
+
+    print(result[['BM_Ratio_Fiscal', 'BM_Ratio_Calendar']].describe())
+    print(result[result.BM_Ratio_Fiscal == result.BM_Ratio_Fiscal.min()])
+    print(result[result.BM_Ratio_Fiscal == result.BM_Ratio_Fiscal.max()])
+    print(result[result.BM_Ratio_Calendar == result.BM_Ratio_Calendar.min()])
+    print(result[result.BM_Ratio_Calendar == result.BM_Ratio_Calendar.max()])
+
+
 if __name__ == '__main__':
-    calculate_yearly()
+    #calculate_yearly()
+    check_yearly()
