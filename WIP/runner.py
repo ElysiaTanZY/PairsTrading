93ed6b1d19e7aee8e46ff89e48245663333c833c
ElysiaTanZY <elysiatan97@gmail.com>
2021-01-28 11:41:57 +0800

    Add standardisation of share list for baseline and clustering

--- WIP/runner.py
+++ WIP/runner.py
@@ -1,26 +1,24 @@
 import json
 import pandas as pd
 import math
-from WIP import baseline, trade, analyse_returns
+import re
+from WIP import baseline, trade, analyse_returns, pre_process_data, dbscan
 
 # Data Files
-'''
-exchange_1 = pd.read_csv("../Updated/Exchange1update.csv")
-exchange_1["date"] = exchange_1.Date.apply(lambda x: exchange_1.to_datetime(x).strftime('%d/%m/%Y %H:%M')).dt.date
-
-exchange_2 = pd.read_csv("../Updated/Exchange2update.csv")
-exchange_2["date"] = exchange_2.Date.apply(lambda x: exchange_2.to_datetime(x).strftime('%d/%m/%Y %H:%M')).dt.date
-
-exchange_3 = pd.read_csv("../Updated/Exchange3update.csv")
-exchange_3["date"] = exchange_3.Date.apply(lambda x: exchange_3.to_datetime(x).strftime('%d/%m/%Y %H:%M')).dt.date
-'''
-
 date_cols = ['date']
-data = pd.read_csv("/Users/elysiatan/PycharmProjects/thesis/Updated/Data_NASDAQ.csv", parse_dates=date_cols)
+#prices_data = pd.read_csv("/Users/elysiatan/PycharmProjects/thesis/Updated/Data_All.csv", parse_dates=date_cols)
+prices_data = pd.read_csv("/Users/elysiatan/PycharmProjects/thesis/Updated/Data_NASDAQ.csv", parse_dates=date_cols)
+prices_data['PERMNO'] = prices_data['PERMNO'].astype(int)
+
 shares = {} # {PERMNO: {year: (start, end)}}
 trading_calendar = {2000: 252, 2001: 248, 2002: 252, 2003: 252, 2004: 252, 2005: 252, 2006: 251, 2007: 251, 2008: 253,
                    2009: 252, 2010: 252, 2011: 252, 2012: 250, 2013: 252, 2014: 252, 2015: 252, 2016: 252, 2017: 251,
                    2018: 251, 2019: 252}
+
+date_cols = ['datadate']
+#firm_data = pd.read_csv('/Users/elysiatan/PycharmProjects/thesis/Updated/Data_Firm_ALl.csv', parse_dates=date_cols)
+firm_data = pd.read_csv('/Users/elysiatan/PycharmProjects/thesis/Updated/Data_Firm.csv', parse_dates=date_cols)
+firm_data['LPERMNO'] = firm_data['LPERMNO'].astype(int)
 
 
 def consolidate_shares(data):
@@ -30,13 +28,14 @@ def consolidate_shares(data):
 
     try:
         with open('../Backup/result.json') as json_file:
+       #with open('../Backup/result_All.json') as json_file:
             shares = json.load(json_file)
             return
     except FileNotFoundError:
         pass
 
     curr_share = data.iloc[0]['PERMNO']
-    shares[curr_share] = {}
+    shares[str(curr_share)] = {}
     prev_year = data.iloc[0]['date'].year
     start = 0
 
@@ -45,19 +44,19 @@ def consolidate_shares(data):
         curr_year = row['date'].year
 
         if pd.notna(row['PERMNO']) and row['PERMNO'] != curr_share:
-            temp = shares[curr_share]
+            temp = shares[str(curr_share)]
             temp[str(prev_year)] = (start, j - 1)
-            shares[curr_share] = temp
+            shares[str(curr_share)] = temp
 
             prev_year = curr_year
             start = j
             curr_share = row['PERMNO']
-            shares[curr_share] = {}
+            shares[str(curr_share)] = {}
 
         elif curr_year != prev_year:
-            temp = shares[curr_share]
+            temp = shares[str(curr_share)]
             temp[str(prev_year)] = (start, j - 1)
-            shares[curr_share] = temp
+            shares[str(curr_share)] = temp
             prev_year = curr_year
             start = j
 
@@ -71,6 +70,7 @@ def find_shares(start_year):
 
     for permno, dates in shares.items():
         print(permno)
+
         if str(start_year) not in dates.keys() or str(start_year + 1) not in dates.keys() or \
                 str(start_year + 2) not in dates.keys() or str(start_year + 3) not in dates.keys():
             continue
@@ -91,18 +91,23 @@ def find_shares(start_year):
 
             indicator = True
             for j in range(start, end + 1):
+                '''
+                if not bool(re.search('[1-9]+', str(prices_data.iloc[j]['PRC']))):
+                    indicator = False
+                    break
+                '''
+                if not prices_data.iloc[j]['PRC'] > 1.0 or not 100 <= prices_data.iloc[j]['DLSTCD'] < 200 or not int(prices_data.iloc[j]['VOL']) > 0:
+                    #1. Discard the stock when it is delisted during the formation period
+                    #2. Discard the stock when its price is less than $1 to avoid relatively high trading costs and
+                    #complications (Faff et al, 2016)
+                    #3. Discard the stock when its trading volume is 0 to replicate practical trading environments and
+                    #deal with only liquid stocks (Faff et al, 2016)
 
-                if not data.iloc[j]['PRC'] > 1.0 or not 100 <= data.iloc[j]['DLSTCD'] < 200 or not int(data.iloc[j]['VOL']) > 0:
-                    '''
-                    1. Discard the stock when it is delisted during the formation period
-                    2. Discard the stock when its price is less than $1 to avoid relatively high trading costs and 
-                    complications (Faff et al, 2016) 
-                    3. Discard the stock when its trading volume is 0 to replicate practical trading environments and 
-                    deal with only liquid stocks (Faff et al, 2016)
-                    '''
+              
                     print("Price less than 1 or delisted")
                     indicator = False
                     break
+
 
             if not indicator:
                 is_complete = False
@@ -114,38 +119,89 @@ def find_shares(start_year):
             trading period
             '''
             print("Appending")
-            prices = data.iloc[dates[str(start_year)][0]:dates[str(start_year+2)][1] + 1]
+            prices = prices_data.iloc[dates[str(start_year)][0]:dates[str(start_year + 2)][1] + 1]
             prices.loc[:, 'LOG_PRC'] = prices.apply(lambda row: math.log(row.PRC), axis=1)
 
-            relevant_shares.append([permno, data.iloc[dates[str(start_year+2)][1]]['SICCD'], dates[str(start_year+2)][1], prices])
+            relevant_shares.append([permno, prices_data.iloc[dates[str(start_year + 2)][1]]['SICCD'], dates[str(start_year + 2)][1], prices])
 
     return relevant_shares
 
 
+def standardise_share_list(relevant_shares, clustering_features):
+    standardised_list = []
+    index_mapping = {}
+
+
+    for i in range(0, len(relevant_shares)):
+        print(i)
+        permno = relevant_shares[i][0]
+
+        print(permno)
+        print(type(permno))
+
+        result = clustering_features.loc[clustering_features['permno'] == int(permno)]
+
+        if len(result.values) != 0:
+            index_mapping[permno] = len(standardised_list)
+            standardised_list.append(relevant_shares[i])
+
+    return standardised_list, index_mapping
+
+
 if __name__ == '__main__':
-    consolidate_shares(data)
+    consolidate_shares(prices_data)
 
     baseline_returns = []
     baseline_num_pairs_chosen = []
     baseline_num_pairs_traded = []
 
+    relevant_shares = find_shares(2000)
+    clustering_features = pre_process_data.main(relevant_shares, firm_data, 2000)
+    clustering_features['permno'] = clustering_features['permno'].astype(int)
+
+    standardised_share_list, index_mapping = standardise_share_list(relevant_shares, clustering_features)
+    print(len(standardised_share_list))
+
+    baseline_pairs = baseline.main(prices_data, standardised_share_list)
+    returns_baseline, num_pairs_traded_baseline = trade.trade(baseline_pairs, 2000 + 3, prices_data)
+
+    dbscan_pairs = dbscan.dbscan_main(clustering_features, index_mapping, standardised_share_list)
+    returns_dbscan, num_pairs_traded_dbscan = trade.trade(dbscan_pairs, 2000 + 3, prices_data)
+
+    print("Baseline")
+    print(returns_baseline)
+    print(num_pairs_traded_baseline)
+    print(len(baseline_pairs))
+
+    print("DBSCAN")
+    print(returns_dbscan)
+    print(num_pairs_traded_dbscan)
+    print(len(dbscan_pairs))
+    '''
     for start_year in range(2000, 2017):
         relevant_shares = find_shares(start_year)
+        clustering_features = pre_process_data.main(relevant_shares, firm_data, start_year)
+        clustering_features['permno'] = clustering_features['permno'].astype(int)
+        standardised_share_list, index_mapping = standardise_share_list(relevant_shares, clustering_features)
 
         # Baseline model
-        baseline_pairs = baseline.main(data, relevant_shares)
-        returns, num_pairs_traded = trade.trade(baseline_pairs, start_year + 3, data)
+        baseline_pairs = baseline.main(prices_data, standardised_share_list)
+        returns, num_pairs_traded = trade.trade(baseline_pairs, start_year + 3, prices_data)
         baseline_returns.append(returns)
         baseline_num_pairs_traded.append(num_pairs_traded)
         baseline_num_pairs_chosen.append(len(baseline_pairs))
+
+        # ML Models
+        #clustering_features = pre_process_data.main(relevant_shares, firm_data, trading_calendar, start_year)
 
     result_baseline = {}
     result_baseline['returns'] = baseline_returns
     result_baseline['pairs_chosen'] = baseline_num_pairs_chosen
     result_baseline['pairs_traded'] = baseline_num_pairs_traded
 
-    file_name = '../Backup/returns_baseline.json'
+    file_name = '../Backup/returns_baseline_All.json'
     with open(file_name, 'w') as fp:
         json.dump(result_baseline, fp)
 
     analyse_returns.analyse_returns(baseline_returns, baseline_num_pairs_traded, baseline_num_pairs_chosen)
+    '''
