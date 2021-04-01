6f9383d064eb1aebac40396a56c84f5ebc2ec065
ElysiaTanZY <elysiatan97@gmail.com>
2021-04-01 09:56:14 +0800

    Add comments for functions in pre_process_data.py

--- WIP/pre_process_data.py
+++ WIP/pre_process_data.py
@@ -1,29 +1,46 @@
-from sklearn import preprocessing
-from sklearn.decomposition import PCA
-
 import csv
 import matlab.engine
-import numpy as np
 import json
 import pandas as pd
 import re
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
 
 num_features = 15
 
 
-def main(relevant_shares, firm_data, start_year):
+def main(relevant_shares, firm_data, start_year, indicator=None, pca_variation=0.95):
+
+    ''' Gets the final features for each share which will be used for clustering
+
+    :param relevant_shares: Shares qualified for consideration in the formation period
+    :param firm_data: Firm Fundamentals data
+    :param start_year: Start of formation period
+    :param indicator: Whether any features should be excluded
+    (Indicator: 1 - Exclude volume, 2 - Exclude price, 3 - Exclude firm fundamentals)
+    :param pca_variation: Total variation final PCA components are expected to explain
+    :return: New features after PCA is done
+    '''
+
     with open('/Users/elysiatan/PycharmProjects/thesis/WIP/volume.json') as json_file:
-    #with open('/Users/elysiatan/PycharmProjects/thesis/WIP/volume_All.json') as json_file:
         vol_data = json.load(json_file)
 
     extract_time_series_features(relevant_shares)
-    consolidated_features = consolidate_features(firm_data, relevant_shares, vol_data, start_year)
-    chosen_features = pca(consolidated_features)
+
+    consolidated_features = consolidate_features(firm_data, vol_data, start_year, indicator)
+    chosen_features = pca(consolidated_features, pca_variation)
 
     return chosen_features
 
 
 def extract_time_series_features(relevant_shares):
+
+    ''' Converts Price Series to static features
+
+    :param relevant_shares: Shares whose price series is to be converted
+    :return: None
+    '''
+
     eng = matlab.engine.start_matlab()
     time_series_data = []
 
@@ -42,21 +59,34 @@ def extract_time_series_features(relevant_shares):
     eng.quit()
 
 
-def consolidate_features(firm_data, relevant_shares, vol_data, start_year):
+def consolidate_features(firm_data, vol_data, start_year, indicator):
+
+    ''' Formats all the necessary input features into a single dataframe
+
+    :param firm_data: Firm Fundamentals data
+    :param vol_data: Trade volume data
+    :param start_year: Start of formation period
+    :param indicator: Whether any features should be excluded
+    (Indicator: 1 - Exclude volume, 2 - Exclude price, 3 - Exclude firm fundamentals)
+    :return: Formatted dataset with all the necessary input features for each stock
+    '''
+
     col_names = ['permno']
 
     for i in range(0, num_features):
         col_name = 'feature' + str(i)
         col_names.append(col_name)
 
-    time_series = pd.read_csv('/Users/elysiatan/PycharmProjects/thesis/WIP/sparse_features.csv', names=col_names, header=None)
-    time_series['permno'] = time_series['permno'].astype(int)
-    print(time_series.dtypes)
+    if indicator != 2:
+        time_series = pd.read_csv('/Users/elysiatan/PycharmProjects/thesis/WIP/sparse_features.csv', names=col_names, header=None)
+        time_series['permno'] = time_series['permno'].astype(int)
+    else:
+        permno_series = pd.read_csv('/Users/elysiatan/PycharmProjects/thesis/WIP/sparse_features.csv', names=col_names, header=None)
+        time_series = permno_series['permno'].to_frame()
 
     for i in range(start_year, start_year+3):
         roa = []
         roe = []
-        #roic = []
         beta = []
         market_cap = []
         b_m_ratio = []
@@ -65,10 +95,7 @@ def consolidate_features(firm_data, relevant_shares, vol_data, start_year):
         for j, row in time_series.iterrows():
             permno = int(row['permno'])
 
-            print(permno)
-            print(type(permno))
-
-            # Get ROA, ROE, ROIC, Market Capitalisation, B/M Ratio, beta
+            # Get ROA, ROE, Market Capitalisation, B/M Ratio, beta
             result = firm_data.loc[firm_data['LPERMNO'] == permno]
             if len(result.values) != 0:
                 result = result[result['datadate'].dt.year == i]
@@ -77,7 +104,6 @@ def consolidate_features(firm_data, relevant_shares, vol_data, start_year):
                 continue
 
             if len(result.values != 0):
-                print("Hello")
                 if bool(re.search('[1-9]+', str(result['roa'].values[0]))) and \
                         bool(re.search('[1-9]+', str(result['roe'].values[0]))) and \
                         bool(re.search('[1-9]+', str(result['marketCap_Calendar'].values[0]))) and \
@@ -86,12 +112,9 @@ def consolidate_features(firm_data, relevant_shares, vol_data, start_year):
 
                     roa.append(result['roa'].values[0])
                     roe.append(result['roe'].values[0])
-                    #roic.append(result['roic'].values[0])
                     market_cap.append(result['marketCap_Calendar'].values[0])
                     b_m_ratio.append(result['BM_Ratio_Fiscal'].values[0])
                     beta.append(result['beta'].values[0])
-
-                    # TODO: Retrieve average trading volume for that year end
                     ave_vol = vol_data[str(permno)][str(i)]
                     vol.append(ave_vol)
 
@@ -100,36 +123,37 @@ def consolidate_features(firm_data, relevant_shares, vol_data, start_year):
             else:
                 time_series.drop(j, inplace=True)
 
-        print(len(roa))
-        print(len(roe))
-        #print(len(roic))
-        print(len(market_cap))
-        print(len(b_m_ratio))
-        print(len(beta))
-        print(len(vol))
-
         roa_col = 'roa' + str(i - start_year)
         roe_col = 'roe' + str(i - start_year)
-        #roic_col = 'roic' + str(i - start_year)
         market_cap_col = 'market_cap' + str(i - start_year)
         b_m_ratio_col = 'b_m_ratio' + str(i - start_year)
         beta_col = 'beta' + str(i - start_year)
         vol_col = 'vol' + str(i - start_year)
 
-        time_series[roa_col] = roa
-        time_series[roe_col] = roe
-        #time_series[roic_col] = roic
-        time_series[market_cap_col] = market_cap
-        time_series[b_m_ratio_col] = b_m_ratio
-        time_series[beta_col] = beta
-        time_series[vol_col] = vol
+        if indicator != 3:
+            time_series[roa_col] = roa
+            time_series[roe_col] = roe
+            time_series[market_cap_col] = market_cap
+            time_series[b_m_ratio_col] = b_m_ratio
+            time_series[beta_col] = beta
+
+        if indicator != 1:
+            time_series[vol_col] = vol
 
     new_file_name = "/Users/elysiatan/PycharmProjects/thesis/Updated/pre-processed.csv"
     time_series.to_csv(new_file_name)
     return time_series
 
 
-def pca(consolidated_features):
+def pca(consolidated_features, pca_variation):
+
+    ''' Conducts PCA
+
+    :param consolidated_features: Initial data frame with all the required features
+    :param pca_variation: Total amount of variation the chosen Principal Components are expected to explain
+    :return: Principal components of each stock formatted in a single data frame
+    '''
+
     col_list = list(consolidated_features.columns)
     col_list.pop()
 
@@ -137,7 +161,7 @@ def pca(consolidated_features):
     features = consolidated_features.loc[:,col_list].values
     features = standard_scaler.fit_transform(features)
 
-    pca_features = PCA(n_components=0.95)
+    pca_features = PCA(n_components=pca_variation)
 
     principalComponents_features = pca_features.fit_transform(features)
     col_list.clear()
@@ -152,15 +176,3 @@ def pca(consolidated_features):
     print(principal_features_Df.head())
 
     return principal_features_Df
-
-    '''
-    col_list.clear()
-    for i in range(0, num_components):
-        col_name = 'component_' + str(i+1)
-        col_list.append(col_name)
-
-    principal_features_Df = pd.DataFrame(data=principalComponents_features, columns=col_list)
-    principal_features_Df['permno'] = consolidated_features['permno']
-
-    print('Explained variation per principal component: {}'.format(pca_features.explained_variance_ratio_))
-    '''
