06e92fc9f82353d3a109f7b86a993ead08843527
ElysiaTanZY <elysiatan97@gmail.com>
2021-04-01 11:42:37 +0800

    Add comments to functions in generate_charts.py

--- WIP/generate_charts.py
+++ WIP/generate_charts.py
@@ -1,66 +1,29 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 import datetime
 
 from matplotlib.dates import DateFormatter
 
 
 def main(identified, traded, fully_invested_returns, model_list):
-    for i in range(0, len(model_list)):
-        generate_bar_chart_pairs(identified[model_list[i]], traded[model_list[i]], model_list[i])
+    for model in model_list:
+        model = str(model)
+        generate_bar_chart_pairs(identified[model], traded[model], model)
 
-    #generate_time_series(fully_invested_returns, model_list)
-    generate_bar_chart_payoffs(fully_invested_returns, model_list)
-
-
-# For returns
-def generate_bar_chart_payoffs(fully_invested_returns, model_list):
-    years = ["0" + str(i) if len(str(i)) == 1 else str(i) for i in range(3, 3 + len(fully_invested_returns[model_list[0]]))]
-
-    ind = np.arange(len(years))  # the label locations
-    width = 0.15  # the width of the bars
-
-    fig, ax = plt.subplots(figsize=(25, 10))
-
-    rects1 = ax.bar(ind, fully_invested_returns["baseline"], width, color='#f29ca2')
-    rects2 = ax.bar(ind + width, fully_invested_returns["kmedoids"], width, color='#61eaf2')
-    rects3 = ax.bar(ind + width * 2, fully_invested_returns["dbscan"], width, color='#6da4d9')
-    rects4 = ax.bar(ind + width * 3, fully_invested_returns["fuzzy"], width, color='#f2bb12')
-
-    # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Fully invested returns')
-    ax.set_xticks(ind)
-    ax.set_xticklabels(years)
-    ax.legend()
-
-    autolabel(rects1, ax)
-    autolabel(rects2, ax)
-    autolabel(rects3, ax)
-    autolabel(rects4, ax)
-
-    ax.legend((rects1[0], rects2[0], rects3[0], rects4[0]), ('Baseline', 'KMedoids', 'DBSCAN', 'Fuzzy C-Means'))
-    plt.show()
+    generate_time_series(fully_invested_returns, model_list)
 
 
-def autolabel(rects, ax):
-    """Attach a text label above each bar in *rects*, displaying its height."""
-
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate('%.3f' % float(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom', fontsize=6)
-
-
-# For pairs identified vs traded
 def generate_bar_chart_pairs(identified_time_series, traded_time_series, model):
-    not_traded_time_series = []
 
-    print(identified_time_series)
-    print(traded_time_series)
+    ''' Plots the bar chart to show the proportion of identified pairs that were traded
+
+    :param identified_time_series: Number of pairs identified by the model in each year
+    :param traded_time_series: Number of pairs traded by the model in each year
+    :param model: Model in concern
+    :return: None
+    '''
+
+    not_traded_time_series = []
 
     if len(identified_time_series) != len(traded_time_series):
         raise Exception("Mismatch in years between time series for identified pairs and traded pairs")
@@ -95,14 +58,31 @@ def generate_bar_chart_pairs(identified_time_series, traded_time_series, model):
 
 
 def generate_time_series(fully_invested_returns, model_list):
+
+    ''' Plots the time series of fully invested returns for each model
+
+    :param fully_invested_returns: List of Fully Invested Returns List for each model
+    :param model_list: Models whose Fully Invested Returns time series should be plotted
+    :return: None
+    '''
+
     years = []
-    for trade_year in range(2003, 2003 + len(fully_invested_returns[model_list[0]])):
+    for trade_year in range(2003, 2003 + len(fully_invested_returns[str(model_list[0])])):
         years.append(pd.to_datetime(datetime.datetime(trade_year, 12, 31)))
 
+    # Plot time series of Fully Invested Returns from each model experimented with, including baseline
     colours = ['springgreen', 'steelblue', 'orange', 'violet']
     for i in range(0, len(model_list)):
-        time_series = pd.Series(fully_invested_returns[model_list[i]], index=years)
-        plt.plot(time_series, label=model_list[i], color=colours[i%len(model_list)])
+        model = str(model_list[i])
+        time_series = pd.Series(fully_invested_returns[model], index=years)
+        plt.plot(time_series, label=model, color=colours[i%len(model_list)])
+
+    # Plot time series of NASDAQ returns to get a sense of how the pairs trading returns correlate with market movement
+    # https://www.macrotrends.net/2623/nasdaq-by-year-historical-annual-returns
+    nasdaq_returns = pd.read_csv("../Backup/nasdaq-by-year-historical-annual-returns.csv")
+    nasdaq_returns_time_series = pd.Series(nasdaq_returns["value"][:len(years)].values, index=years)
+    nasdaq_returns_time_series = nasdaq_returns_time_series.divide(100)
+    plt.plot(nasdaq_returns_time_series, label="NASDAQ", color='black')
 
     date_form = DateFormatter("%y")
     plt.gca().xaxis.set_major_formatter(date_form)
