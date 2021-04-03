b96a404fde92e37cced6162444fd6f50e5c13bb9
ElysiaTanZY <elysiatan97@gmail.com>
2021-04-03 15:34:57 +0800

    Ignore unclassified stocks for the baseline model

--- WIP/baseline.py
+++ WIP/baseline.py
@@ -8,13 +8,13 @@ def main(relevant_shares):
     ''' Identifies pairs to be traded under the Baseline model
 
     :param relevant_shares: Shares to be considered in the formation period
-    :return: Cointegrated pairs separated into their individual groups
+    :return: Cointegrated pairs separated into their individual groups and number of unclassified shares
     '''
 
     print("Forming pairs")
-    grouped_shares = group_shares(relevant_shares)
+    grouped_shares, num_unclassified = group_shares(relevant_shares)
     cointegrated_pairs = cointegration_test.main(grouped_shares)
-    return cointegrated_pairs
+    return cointegrated_pairs, num_unclassified
 
 
 def group_shares(relevant_shares):
@@ -22,7 +22,7 @@ def group_shares(relevant_shares):
     ''' Groups shares according to industry group based on the Fama French classification
 
     :param relevant_shares: Shares to be considered in the formation period
-    :return: Grouped shares
+    :return: Grouped shares and number of unclassified shares
     '''
 
     # Adapted from: https://github.com/alexchinco/CRSP-Data-Summary-Statistics-by-Industry-/blob/master/industries.json
@@ -30,9 +30,9 @@ def group_shares(relevant_shares):
         data = json.load(json_file)
 
     groups = {}
+    num_unclassified = 0
 
     for i in range(0, len(relevant_shares)):
-        print(i)
         sic_code = relevant_shares[i][1]
         if str(sic_code).isalpha():
             sic_code = int(9999)
@@ -40,8 +40,7 @@ def group_shares(relevant_shares):
             sic_code = int(sic_code)
 
         if sic_code == 9999:
-            name = 'unclassified'
-            update_groups(groups, name, relevant_shares, i)
+            num_unclassified += 1
 
         else:
             is_grouped = False
@@ -58,7 +57,7 @@ def group_shares(relevant_shares):
                 if is_grouped:
                     break
 
-    return groups
+    return groups, num_unclassified
 
 
 def update_groups(groups, name, relevant_shares, index):
